6051fe1f5a7d8100046fe7b0f199620c4b434973
Raj Sakalabttula <raj_sakalabttula@epam.com>
2023-11-28 04:54:38 +0530

    Update Jenkinsfile

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -30,9 +30,9 @@ cp target/${NAME}-${VERSION}.jar sample.jar
 
 echo "File created"
 ssh -i $1 dhluser@10.0.0.5 << EOF 
- sudo ls /opt/tomcat/webapps
- sudo mkdir /opt/tomcat/backup/$(date +%F)/$2
- sudo cp /opt/tomcat/webapps/sample.jar /opt/tomcat/backup/$(date +%F)/$2/
- sudo rm -f /opt/tomcat/webapps/sample.jar
+sudo ls /opt/tomcat/webapps
+sudo mkdir -p /opt/tomcat/backup/$(date +%F)/$2
+sudo cp /opt/tomcat/webapps/sample.jar /opt/tomcat/backup/$(date +%F)/$2/
+sudo rm -f /opt/tomcat/webapps/sample.jar
 EOF
  

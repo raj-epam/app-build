7a4f829a5de4f2128ac131098550eb1721153a12
Raj Sakalabttula <raj_sakalabttula@epam.com>
2023-11-28 04:27:13 +0530

    Update Jenkinsfile

--- jenkins/scripts/copy.sh
+++ jenkins/scripts/copy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh         
 sftp -i $1 dhluser@10.0.0.5 << ! 
- sudo ls /opt/tomcat/webapp
- sudo cp /opt/tomcat/webapp/sample.jar /opt/tomcat/backup/$(date)/sample.jar
- rm -f /opt/tomcat/webapp/sample.jar
- put sample.jar /opt/tomcat/webapp/sample.jar
+ sudo ls /opt/tomcat/webapps
+ sudo cp /opt/tomcat/webapps/sample.jar /opt/tomcat/backup/$(date)/sample.jar
+ rm -f /opt/tomcat/webapps/sample.jar
+ put sample.jar /opt/tomcat/webapps/sample.jar
  bye
  !

61bc01795403fa2da3c23d7485703b3545977cd0
Raj Sakalabttula <raj_sakalabttula@epam.com>
2023-11-28 04:11:46 +0530

    Update Jenkinsfile

--- jenkins/scripts/copy.sh
+++ jenkins/scripts/copy.sh
@@ -1,5 +1,5 @@
- #!/bin/sh         
- sftp -i $2 dhluser@10.0.0.5 
+#!/bin/sh         
+sftp -i $1 dhluser@10.0.0.5 
 #  <<EOF sudo ls /opt/tomcat/webapp
 #  sudo cp /opt/tomcat/webapp/simple.war /opt/tomcat/backup/$(date)/simple.war
 #  put ./target/my-app-1.0-SNAPSHOT.jar /opt/tomcat/webapp/simple.war
--- lb-node-mantian.sh
+++ lb-node-mantian.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 # https://my.f5.com/manage/s/article/K13310 for refernece turning node offline and online
 
-if [ $2 == "disable" ]
-then
+# if [ $2 == "disable" ]
+# then
 
-    tmsh
-    modify /ltm node $3 session user-disabled
-    echo "$3 node has been disabled"
-    save /sys config
-elif [ $2 == "enable" ]
-then
-    tmsh
-    modify /ltm node $3 session user-disabled
-    echo "$3 node has been enabled"
-    save /sys config
-fi
-
+#     tmsh
+#     modify /ltm node $3 session user-disabled
+#     echo "$3 node has been disabled"
+#     save /sys config
+# elif [ $2 == "enable" ]
+# then
+#     tmsh
+#     modify /ltm node $3 session user-disabled
+#     echo "$3 node has been enabled"
+#     save /sys config
+# fi
+echo $1
+echo $2
+echo $3
+echo $4

ea2c6fbe56e735a5a846e61606b89550085a1631
Raj Sakalabttula <raj_sakalabttula@epam.com>
2023-11-28 04:17:08 +0530

    Update Jenkinsfile

--- jenkins/scripts/copy.sh
+++ jenkins/scripts/copy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh         
-sftp -i $1 dhluser@10.0.0.5 <<EOF sudo ls /opt/tomcat/webapp
+sftp -i $1 dhluser@10.0.0.5 << ! 
+ sudo ls /opt/tomcat/webapp
  sudo cp /opt/tomcat/webapp/simple.war /opt/tomcat/backup/$(date)/simple.war
  put ./target/my-app-1.0-SNAPSHOT.jar /opt/tomcat/webapp/simple.war
- exit
- EOF
+ bye
+ !
